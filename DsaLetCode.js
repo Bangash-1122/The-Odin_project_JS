8ee7a5741412204a5f661ba723b857902aa85fdf
Ubaid_Bangash <ubaidkhan0077800@gmail.com>
2026-08-26 22:32:42 -0400

    Create DsaLetCode.js

---
+++ DsaLetCode.js
@@ -0,0 +1,17 @@
+
+//check out the discount 
+
+let amount  = Number(prompt('Your cureent electric bills', '')),
+  let dis = 0
+  if(amount> 0 && amount <= 5000){
+    dis = 0;
+  } else if(amount > 5000 && amount <= 7000){
+    dis = 5
+  }
+  } else if(amount > 7000 && amount <= 9000){
+     dis = 10
+  }
+  } else if(amount > 9000){
+   dis = 20
+  }
+    console.log(amount - Math.floor(dis*amount/100));
