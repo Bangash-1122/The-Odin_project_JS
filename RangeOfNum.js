a4fdf6f045789e4bd332af44ac531c86f2075c1e
Ubaid_Bangash <ubaidkhan0077800@gmail.com>
2026-08-31 10:08:36 -0400

    Create RangeOfNum.js

---
+++ RangeOfNum.js
@@ -0,0 +1,13 @@
+
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+
+// put your number and then check your answer
+
